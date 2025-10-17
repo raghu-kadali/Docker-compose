91d5b74367877c97e79ee4e64bb421232b6166eb
raghu-kadali <raghu2.kadali@gmail.com>
2025-10-17 15:10:55 +0530

    CHANGE TO RAGHU

--- node-app/app.js
+++ node-app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my first Dockerized Node.js app! (FROM NSV)');
+  res.send('Hello from my first Dockerized Node.js app! (FROM RAGHU-KADALI)');
 });
 
 app.listen(port, () => {
